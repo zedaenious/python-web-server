91b99be6270e26171d7abdab369a18c8ee85ef7a
zedaenious <zed.dev.22@gmail.com>
2023-11-16 00:30:06 -0500

    users route get working

--- server.py
+++ server.py
@@ -1,22 +1,24 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
 
-# import pandas as pd
-# import ast
+import pandas as pd
+import ast
 
 app = Flask(__name__)
 api = Api(app)
 
 class Users(Resource):
-  # methods go here
-  pass
-
+  def get(self):
+    data = pd.read_csv('users.csv')
+    data = data.to_dict()
+    return {'data': data}, 200
+    
 class Locations(Resource):
-  # methods go here
-  pass
-
-api.add_resource(Users, '/users')
-api.add_resource(Locations, '/locations')
+    # methods go here
+    pass
+    
+api.add_resource(Users, '/users')  # '/users' is our entry point for Users
+api.add_resource(Locations, '/locations')  # and '/locations' is our entry point for Locations
 
 if __name__ == '__main__':
-  app.run()
+    app.run()  # run our Flask app
